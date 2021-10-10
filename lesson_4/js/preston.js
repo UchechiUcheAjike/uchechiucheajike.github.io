be4c28ceec805d51324f6901d520cf3fe20c2faf
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2021-10-10 17:54:26 -0600

    fixed javascript

--- lesson_4/js/preston.js
+++ lesson_4/js/preston.js
@@ -1,25 +1,5 @@
 // get last updated time
 
-window.addEventListener('load', () =>{
-
-lastModified();
-
-
- })
-
-function lastModified() 
-{
-     const span = document.querySelector('#updated');
-     var date = new Date();
-    span.innerHTML= String(document.lastModified);}
-    span.innerHTML= String(date.getDay() +" "+ date.getDate() +" "+ date.getMonth() +" "+ date.getFullYear());
-
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-document.getElementById("demo").innerHTML = months[d.getMonth()];
-
-const today = new Date() today. toLocaleString('default', { month: 'long' })
-today. toLocaleString('default', { month: 'short' })
-const today = new Date() today. toLocaleString('it-IT', { month: 'long' })
 
 // hamburger javascript code
 const hambutton = document.querySelector('.ham');
