569b206fb20144b7e21970bc13a4aa8a1e0bbc28
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2021-10-15 12:59:00 -0600

    comments, hamburger menu, time format

--- lesson_4/js/preston.js
+++ lesson_4/js/preston.js
@@ -1,21 +1,10 @@
-// // get last updated time
 
-
-// // hamburger javascript code
-// const hambutton = document.querySelector('.ham');
-// const mainnav = document.querySelector('.navigation')
-
-// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// // To solve the mid resizing issue with responsive class on
-// window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')}
-
-
+// function for hamburger
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-
+// function for date format
 function lastModified() {
     // var x = document.write(new Date(document.lastModified));
     today = new Date(document.lastModified);
