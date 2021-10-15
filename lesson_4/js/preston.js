a057b69c65c0835881b3098a5f57bc4f9c73ba02
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2021-10-15 12:27:40 -0600

    fixed my hamburger menu

--- lesson_4/js/preston.js
+++ lesson_4/js/preston.js
@@ -1,11 +1,16 @@
-// get last updated time
+// // get last updated time
 
 
-// hamburger javascript code
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+// // hamburger javascript code
+// const hambutton = document.querySelector('.ham');
+// const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+// // To solve the mid resizing issue with responsive class on
+// window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')}
+
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
